198d92c19fb557c4e70a69665e5e8c5d675e0a21
Zsuzsanna Szeredi <zszeredi@e-r3-p7.s19.be>
2019-11-05 14:01:45 +0100

    read checks

--- read_file.c
+++ read_file.c
@@ -1,3 +1,10 @@
+#include "libft/libft.h"
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <limits.h>
+
 int		read_file(const int fd)
 {
 	int ret;
@@ -8,8 +15,17 @@ int		read_file(const int fd)
 	while ((ret = read(fd, buf, 21)))
 	{
 		buf[ret] = '\0';
-		file_checker();
+	//	file_checker();
 	}
+	ft_putstr(buf);
 	return (1);
 }
+int main ()
+{
+	int fd;
 
+	fd = open("tests/invalid_single/invalid_single_6", O_RDONLY);
+
+	read_file(fd);
+	return (0);
+}	
