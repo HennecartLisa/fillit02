dbf72b5bf2ee6b7038239daf2cd65b4c8aa5e776
Zsuzsanna Szeredi <zszeredi@e-r4-p6.s19.be>
2019-11-08 16:00:33 +0100

    read_file

--- read_file.c
+++ read_file.c
@@ -1,11 +1,12 @@
 #include "fillit.h"
+#include <stdio.h>//
 
 //started function to check for '#', '.', and '\n'.
-int		ft_checl_nl(char **str, int i)
+int		ft_check_nl(char **str, int i)
 {
-		if (str[i][5] != '\n' || str[i][9] != '\n' || str[i][14] != '\n' || str[i][19] != '\n' || str[i][20] != '\n')
-			return (-1);
-		return(0);
+	if (str[i][5] != '\n' || str[i][9] != '\n' || str[i][14] != '\n' || str[i][19] != '\n' || str[i][20] != '\n')
+		return (1);
+	return(0);
 }
 
 int		ft_check_charachters(char **str, int i)
@@ -22,21 +23,21 @@ int		ft_check_charachters(char **str, int i)
 	ft_putstr(str[i]);
 	while (str[i][j] != '\0')
 	{
+		//		printf("j = %c\n", str[i][j]);
 		if (str[i][j] == '#')
 			counter1++;
 		if (str[i][j] == '.')
 			counter2++;
 		if (str[i][j] == '\n')
 			counter3++;
+
 		j++;
-	}
+	}	
+	//	printf("1: %d -- 2: %d -- 3: %d\n", counter1, counter2, counter3);
 	str[i][j] = '\0';
 	if (counter1 != 4 || counter2 != 12 || counter3 != 5)
-	{
-		ft_putnbr(counter1);
-		return (-1);
-	}
-		return (0);
+		return (1);
+	return (ft_check_nl(&str[i], i));
 }
 //have to make it go in a loop until the whole file is read//
 int		read_file(const int fd)
@@ -46,19 +47,23 @@ int		read_file(const int fd)
 	char **str;
 	int i;
 	int j;
+	int number_tetros;
+
+	number_tetros = 0;
 
 	i = -1;
 	j = 0;
 	if(!(str = (char **)malloc(sizeof(char *) * BUFFER_SIZE + 1)))
-		return (-1);
+		return (1);
 	if ((fd < 0 || fd >= OPEN_MAX) || read(fd, buf, 0) < 0)
-		return (-1);
-	ret = read(fd, buf, BUFFER_SIZE);
-	buf[ret] = '\0';	
-	//	ft_putstr(buf);
-	str[++i] = ft_strdup(buf);
-	//	ft_putstr(str[1]);
-	ft_check_charachters(&str[i], i);
-	ft_check_nl(&str[i], i);
+		return (1);
+	while ((ret = read(fd, buf, BUFFER_SIZE)) /*|| number_tetros >= 1 || number_tetros <= 26 */ )
+		buf[ret] = '\0';	
+		//	ft_putstr(buf);
+		str[++i] = ft_strdup(buf);
+		//	ft_putstr(str[1]);
+		ft_check_charachters(&str[i], i);
+		number_tetros++;
+	//	printf("%s\n", str[1]);
 	return (0);
 }
