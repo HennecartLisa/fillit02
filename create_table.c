836ef6c5d22620bb60594f5d7add3723a942a89f
Zsuzsanna Szeredi <zszeredi@w-r5-p5.s19.be>
2019-11-28 14:25:17 +0100

    create_table with malloc protections

--- create_table.c
+++ create_table.c
@@ -6,32 +6,28 @@
 /*   By: zszeredi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 16:49:23 by zszeredi          #+#    #+#             */
-/*   Updated: 2019/11/28 14:00:25 by zszeredi         ###   ########.fr       */
+/*   Updated: 2019/11/28 14:24:50 by zszeredi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdio.h>
 
-// Create_table. We derive the number of tetroes from storage.c, based on that we calculate the minimum size of the table and then we allocate the space for it. 
- 
-int ft_sqrt(int nb)
+// Create_table. We derive the number of tetroes from storage.c, based on that we calculate the minimum size of the table and then we allocate the space for it.
+
+int		ft_sqrt(int nb)
 {
 	int square;
 
 	square = 1;
-	ft_putchar('a');
 	while (nb > 1 && square <= nb / 2)
 	{
-		if 	(square * square >= nb)
+		if (square * square >= nb)
 		{
-			ft_putchar('y');
-return(square);
+			return (square);
 		}
 		square++;
 	}
-
-	ft_putchar('y');
 	return (2);
 }
 
@@ -40,22 +36,35 @@ int		min_table(int i)
 	int size;
 
 	size = ft_sqrt(i * 4);
-		return (size);
+	return (size);
+}
+
+t_table	*ltg(char *str)
+{
+	ft_strdel(&str);
+	return (NULL);
 }
 
 t_table	*ft_allocate(t_tetra *s)
 {
 	int		i;
-	int		size;
 	t_table *tab;
 
 	i = 0;
-	size = min_table(s->total_tetroes);
-	tab = (t_table *)malloc(sizeof(t_table));
-	(*tab).square = ft_memalloc(size * sizeof(char *));
-	while (i < size)
+	if (!(tab = (t_table *)malloc(sizeof(t_table))))
 	{
-		tab->square[i] = ft_memalloc((size * sizeof(char)));
+		free(tab);
+		tab = NULL;
+	}
+	tab->nb_tetroes = min_table(s->total_tetroes);
+	ft_putchar('\n');
+	ft_putnbr(tab->nb_tetroes);
+	if (!((*tab).square = ft_memalloc(tab->nb_tetroes * sizeof(char *))))
+		return (ltg(*(*tab).square));
+	while (i < tab->nb_tetroes)
+	{
+		if (!(tab->square[i] = ft_memalloc((tab->nb_tetroes * sizeof(char)))))
+			return (ltg(*(tab->square)));
 		i++;
 	}
 	//printf("%s\n", tab->square[i]);
