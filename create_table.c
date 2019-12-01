6dfc5e174e7f924197ab7ad4cf2048f492142d97
Zsuzsanna Szeredi <zszeredi@w-r7-p5.s19.be>
2019-12-01 16:36:25 +0100

    added size

--- create_table.c
+++ create_table.c
@@ -6,7 +6,7 @@
 /*   By: zszeredi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 16:49:23 by zszeredi          #+#    #+#             */
-/*   Updated: 2019/11/28 19:31:12 by zszeredi         ###   ########.fr       */
+/*   Updated: 2019/12/01 16:17:31 by zszeredi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ t_table	*ltg(char *str)
 	return (NULL);
 }
 
-t_table	*ft_allocate(t_tetra *s)
+t_table	*ft_allocate(t_tetra *s, int size)
 {
 	int		i;
 //	int k = 0;
@@ -59,11 +59,11 @@ t_table	*ft_allocate(t_tetra *s)
 		free(table);
 		table = NULL;
 	}
-	table->nb_tetroes = min_table(s->total_tetroes);
-	ft_putstr("nb_tetroes is:");
-	ft_putnbr(table->nb_tetroes);
-	ft_putchar('\n');
-	if (!((*table).square = ft_memalloc(table->nb_tetroes * sizeof(char *))))
+	table->nb_tetroes = min_table(s->total_tetroes + size);
+//	ft_putstr("nb_tetroes is:");
+//	ft_putnbr(table->nb_tetroes);
+//	ft_putchar('\n');
+	if (!((*table).square = ft_memalloc((table->nb_tetroes * sizeof(char *)))))
 		return (ltg(*(*table).square));
 	while (i <= table->nb_tetroes)
 	{
@@ -71,10 +71,10 @@ t_table	*ft_allocate(t_tetra *s)
 			return (ltg(*(table->square)));
 		i++;
 	}
-	ft_putstr("nb_tetroes now is:");
+	ft_putstr("min_size now is:");
 	ft_putnbr(table->nb_tetroes);
-//	ft_putchar('\n');
-	place(table, s, s->total_tetroes);
+	ft_putchar('\n');
+	place(table, s, 0);
 	//printf("%s\n", tab->square[i]);
 	return (table);
 }
