be789d6a81ba3d7ce63a756f77f8c4fa58d8e96e
Lisa Hennecart <hennecart.lisa@gmail.com>
2019-12-19 17:16:04 +0100

    backtracking back

--- backtracking.c
+++ backtracking.c
@@ -1,65 +1,153 @@
-#include "fillit.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   baktracking.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zszeredi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/12 10:35:22 by zszeredi          #+#    #+#             */
+/*   Updated: 2019/12/15 17:49:03 by zszeredi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-/*void	main_insert(t_table *s2, t_tetra *s, int size)
+#include "fillit.h"
+#include <stdio.h>
+
+/*int     verif(t_tetra *s, t_table *s2, int x, int y, int placeit)
 {
-	int x = 0;
-	int i = 0;
-	int nb = 0;
-	int result;
-	int y = 0;
-	ft_putstr("trigger insert");
-	main_if_possible(s2, s, nb);
+	int     i;
+	int     j;
+
+	i = 0;
+	if (placeit == -1)
+		return (0);
+	while (x + i < s2->table_size && i < 4)
 	{
-		result = insert_tetro(x, i, nb, s->letter, s2, s);
-		if (result < 1)
+		j = 0;
+		while (y + j < s2->table_size && j < 4)
 		{
+			if (s->tab[i][j] == 1)
+			{
+				if (s2->square[x + i][y + j] == '.')
+					placeit ? s2->square[x + i][y + j] = s->letter : 0;
+				else
+					return (-1);
+
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+int		tetri_del(t_tetra *s, t_table *s2 ,int x ,int y)
+{
+	int     i;
+	int     j;
+
+	i = 0;
+	while (x + i < s2->table_size && i < 4)
+	{
+		j = 0;
+		while (y + j < s2->table_size && j < 4)
+		{
+			if (s2->square[x + i][y + j] == s->letter)
+				s2->square[x + i][y + j] = '.';
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}*/
+void	delete_table(t_table *s2)
+{
+	int i;
+	i = 0;
+
+	while (i < s2->table_size)
+	{
+		ft_strdel(&s2->square[i]);
+		i++;
+	}
+	ft_strdel(s2->square);
+	free(s2);
+	s2 = NULL;
+}
+/*
+ * char **map == t_table *s2
+ * t_tetriminos *tetri == t_tetra *s
+ */
+//increment table ,what if table is too small needs end of the table
+int		solver(t_table *s2, t_tetra *s)
+{
+	int  x;
+	int  y;
+	int nb;
+	int end;
+	//	int i = 0;
+	//	int j = 0;
+	static int size = 0;
+	nb = 0;
+	x = 0;
+	//		ft_print_tetros(s);
+	//	ft_print_table(s2);
+	//if (!s) // condition for the end of the table
+	//	return (0);
+	//	while (x <= s2->table_size)
+	//	{
+	y = 0;
+	//	while (y <= 4) // s2->table_size)
+	//	{
+	end = place(s2, s, nb, s->letter);
+	ft_putnbr(end);
+//	ft_print_table(s2);
+	ft_putstr("i am back to solver\n");	
+
+	nb++;
+	ft_putnbr(nb);
+	{
+		if (end  == -1)//not enough space //verif(s, s2, x, y, 0)))
+		{	
+			ft_putstr("end is -1\n");	
 			delete_table(s2);
 			ft_allocate(s, ++size);
 		}
-		if (result == 0)
+		if (end == 0)//position already has char
 		{
-			if (nb == s->total_tetroes)
-				s2->square[x][y] = '\0';
-			if (nb < s->total_tetroes)
+			ft_putstr("end is 0\n");	
+			return (-1);
+		}
+		if (end == 1)
+		{
+			ft_putstr("end is 1\n");	
+			if (nb < s->total_tetroes)//if I still have tetroes
 			{
-				main_if_possible(s2, s, nb);
-				insert_tetro(x, i, nb++, s->letter++, s2 ,s);
+				ft_putstr("i have to go to next tetro\n");	
+				s->letter++;
+				end = place(s2, s, nb, s->letter);
 			}
-		}	
-		ft_print_table(s2);
-	}	
-}*/
-
-int		main_if_possible(t_table *s2, t_tetra *s, int size)
-{
-	static int x= 0;
-	int result;
-	int nb = 0;
-	ft_putstr("starting");
-	//ft_print_table(s2);
-	while (x < s2->table_size)
-	//	if (check_if_possible(s2, x, s, nb) < 1)
-		result = check_if_possible(s2, x, s, nb);	
-	
-	if (result == 1)
-	{
-
-		ft_putstr("result is 1");
-		if (x < s2->table_size)
-		{
-		ft_putstr("moving to next line");
-			x++;
-		}	
-		if ( x == s2->table_size)
-		{
-			ft_putstr("end of tetro");
+			else //for last line
+			{
+				ft_putstr("end of file, I am done");
+				s2->square[x][y] = '\0';
 				return (1);
 			}
-	}
-		if (result  == -1)
-		{
-			delete_table(s2);
-			ft_allocate(s, ++size);
 		}
-	return (1);
+		//	if (place(s, s2, x, nb) == 0)
+		//if 0, already something there move away 
+		//	if (place(s, s2, x, nb) == 1)
+		//go to next tetro
+		//				if (solver(s2, s + 1))
+		//					return (1);
+		//	else
+		//	tetri_del(s, s2, x, y);
+
+		//	}
+		//	y++;
+		//	ft_print_tetros(s);
+		//	ft_print_table(s2);	
+}
+//	}
+//x++	
+return (1);
 }
